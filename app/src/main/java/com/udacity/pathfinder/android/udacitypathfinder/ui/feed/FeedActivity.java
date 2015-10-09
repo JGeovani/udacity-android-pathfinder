693a6802e97112b56645b35f1dba76bb812030f9
mjhassanpur <mjhassanpur@gmail.com>
2015-10-09 15:06:02 -0700

    Replace query adapter with simple query

--- app/src/main/java/com/udacity/pathfinder/android/udacitypathfinder/ui/feed/FeedActivity.java
+++ app/src/main/java/com/udacity/pathfinder/android/udacitypathfinder/ui/feed/FeedActivity.java
@@ -2,15 +2,32 @@ package com.udacity.pathfinder.android.udacitypathfinder.ui.feed;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.udacity.pathfinder.android.udacitypathfinder.R;
+import com.udacity.pathfinder.android.udacitypathfinder.parse.Article;
+
+import java.util.List;
 
 public class FeedActivity extends AppCompatActivity {
+
+  private static final String TAG = "FeedActivity";
+  private static final String ARTICLES_LABEL = "Articles";
+  private static final String CHECKED_CLOUD_KEY = "checkedCloud";
+
+  private RecyclerView recyclerView;
+  private FeedAdapter feedAdapter;
+  private boolean checkedCloud;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -25,9 +42,55 @@ public class FeedActivity extends AppCompatActivity {
     tabLayout.addTab(tabLayout.newTab().setText(res.getString(R.string.title_tab_grid)));
     tabLayout.addTab(tabLayout.newTab().setText(res.getString(R.string.title_tab_list)));
 
-    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+    feedAdapter = new FeedAdapter();
+    recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
     recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    recyclerView.setAdapter(new FeedAdapter(this, recyclerView));
+    recyclerView.setAdapter(feedAdapter);
 
+    ParseQuery<Article> query = ParseQuery.getQuery(ARTICLES_LABEL);
+    query.fromLocalDatastore();
+    query.findInBackground(callback);
   }
+
+  @Override protected void onSaveInstanceState(Bundle outState) {
+    outState.putBoolean(CHECKED_CLOUD_KEY, checkedCloud);
+    super.onSaveInstanceState(outState);
+  }
+
+  @Override protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    checkedCloud = savedInstanceState.getBoolean(CHECKED_CLOUD_KEY);
+  }
+
+  private final FindCallback<Article> callback = new FindCallback<Article>() {
+    @Override public void done(List<Article> list, ParseException e) {
+      if (e == null) {
+        if (list.isEmpty() && !checkedCloud) {
+          Log.d(TAG, "Local datastore is empty, checking the cloud...");
+          ParseQuery<Article> query = ParseQuery.getQuery(ARTICLES_LABEL);
+          query.findInBackground(new FindCallback<Article>() {
+            @Override public void done(final List<Article> list, ParseException e) {
+              checkedCloud = true;
+              if (e == null) {
+                Log.d(TAG, list.size() + " results found in the cloud");
+                if (!list.isEmpty()) {
+                  ParseObject.pinAllInBackground(ARTICLES_LABEL, list);
+                  feedAdapter.updateArticles(list);
+                }
+              } else {
+                Log.e(TAG, "Error occurred while attempting to retrieve data from the cloud", e);
+              }
+            }
+          });
+        } else if (!list.isEmpty()) {
+          Log.d(TAG, list.size() + " results found in local datastore");
+          feedAdapter.updateArticles(list);
+        } else {
+          Log.d(TAG, "Local datastore empty. Cloud is empty as well or simply could not connect");
+        }
+      } else {
+        Log.e(TAG, "Error occurred while attempting to retreive data from local datastore", e);
+      }
+    }
+  };
 }
--- app/src/main/java/com/udacity/pathfinder/android/udacitypathfinder/ui/feed/FeedAdapter.java
+++ app/src/main/java/com/udacity/pathfinder/android/udacitypathfinder/ui/feed/FeedAdapter.java
@@ -1,6 +1,5 @@
 package com.udacity.pathfinder.android.udacitypathfinder.ui.feed;
 
-import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,77 +7,50 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.parse.ParseQueryAdapter;
-import com.squareup.picasso.Picasso;
 import com.udacity.pathfinder.android.udacitypathfinder.R;
 import com.udacity.pathfinder.android.udacitypathfinder.parse.Article;
 
+import java.util.Collections;
 import java.util.List;
 
 public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.FeedViewHolder> {
-  private ParseQueryAdapter<Article> parseAdapter;
-  private FeedAdapter thingsAdapter = this;
-  private ViewGroup parseParent;
 
-  public FeedAdapter(Context context, ViewGroup parentIn) {
-    parseParent = parentIn;
-    parseAdapter = new ParseQueryAdapter<Article>(context, "Articles") {
+  private List<Article> articles = Collections.emptyList();
 
-      @Override
-      public View getItemView(Article article, View view, ViewGroup parent) {
-        if (view == null) {
-          view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_article, parent, false);
-        }
-        super.getItemView(article, view, parent);
-
-        ImageView image = (ImageView) view.findViewById(R.id.article_image);
-        TextView title = (TextView) view.findViewById(R.id.article_title);
-        TextView description = (TextView) view.findViewById(R.id.article_beginning);
-
-        Picasso.with(getContext()).load(article.getImageUrl()).into(image);
-        title.setText(article.getTitle());
-        description.setText(article.getDescription());
-
-        return view;
-      }
-    };
-    parseAdapter.addOnQueryLoadListener(new OnQueryLoadListener());
-    parseAdapter.loadObjects();
+  @Override public FeedViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+    LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+    return new FeedViewHolder(inflater.inflate(R.layout.list_item_article, parent, false));
   }
 
-  @Override
-  public FeedViewHolder onCreateViewHolder(ViewGroup parent, int i) {
-    View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_article, parent, false);
-    return new FeedViewHolder(v);
+  @Override public void onBindViewHolder(FeedViewHolder holder, int pos) {
+    if (articles != null && !articles.isEmpty()) {
+      Article article = articles.get(pos);
+      holder.image.setImageResource(R.mipmap.ic_launcher);
+      holder.title.setText(article.getTitle());
+      holder.description.setText(article.getDescription());
+    }
   }
 
-
-  @Override
-  public void onBindViewHolder(FeedViewHolder holder, int i) {
-    parseAdapter.getView(i, holder.view, parseParent);
+  @Override public int getItemCount() {
+    return articles.size();
   }
 
-  @Override
-  public int getItemCount() {
-    return parseAdapter.getCount();
+  public void updateArticles(List<Article> articles) {
+    this.articles = articles;
+    notifyDataSetChanged();
   }
 
   public static class FeedViewHolder extends RecyclerView.ViewHolder {
-    public View view;
 
-    public FeedViewHolder(View v) {
-      super(v);
-      view = v;
-    }
-  }
+    private ImageView image;
+    private TextView title;
+    private TextView description;
 
-  public class OnQueryLoadListener implements ParseQueryAdapter.OnQueryLoadListener<Article> {
-
-    public void onLoading() {
-    }
-
-    public void onLoaded(List<Article> objects, Exception e) {
-      thingsAdapter.notifyDataSetChanged();
+    public FeedViewHolder(View view) {
+      super(view);
+      image = (ImageView) view.findViewById(R.id.article_image);
+      title = (TextView) view.findViewById(R.id.article_title);
+      description = (TextView) view.findViewById(R.id.article_description);
     }
   }
 }
