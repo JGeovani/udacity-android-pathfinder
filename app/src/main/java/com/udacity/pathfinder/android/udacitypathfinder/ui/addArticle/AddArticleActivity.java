801fad2b8ee329ca5fe45cfe94d7a37d889d0c85
mjhassanpur <mjhassanpur@gmail.com>
2015-11-04 21:07:03 -0800

    Add save article feature

--- app/src/main/java/com/udacity/pathfinder/android/udacitypathfinder/ui/addArticle/AddArticleActivity.java
+++ app/src/main/java/com/udacity/pathfinder/android/udacitypathfinder/ui/addArticle/AddArticleActivity.java
@@ -1,17 +1,26 @@
 package com.udacity.pathfinder.android.udacitypathfinder.ui.addArticle;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.udacity.pathfinder.android.udacitypathfinder.R;
+import com.udacity.pathfinder.android.udacitypathfinder.data.ParseConstants;
+import com.udacity.pathfinder.android.udacitypathfinder.data.models.Article;
 import com.udacity.pathfinder.android.udacitypathfinder.ui.feed.FeedActivity;
+
+import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.BindString;
@@ -24,11 +33,15 @@ public class AddArticleActivity extends AppCompatActivity {
 
   @BindString(R.string.title_activity_add_article) String ADD_ARTICLE_ACTIVITY_TITLE;
   @Bind(R.id.toolbar) Toolbar toolbar;
-  @Bind(R.id.nanodegrees) TextView nanodegrees;
+  @Bind(R.id.article_title) EditText titleEditText;
+  @Bind(R.id.article_url) EditText articleUrlEditText;
+  @Bind(R.id.image_url) EditText imageUrlEditText;
+  @Bind(R.id.nanodegrees) TextView nanodegreesTextView;
 
-  @State Integer[] selections = new Integer[]{};
+  @State Integer[] selectionIndices = new Integer[]{};
   @State String selectionsText;
 
+  private static final String DELIMITER = ", ";
   private MaterialDialog nanodegreesDialog;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +53,7 @@ public class AddArticleActivity extends AppCompatActivity {
     toolbar.setTitle(ADD_ARTICLE_ACTIVITY_TITLE);
     setSupportActionBar(toolbar);
 
-    if (selectionsText != null) nanodegrees.setText(selectionsText);
+    if (selectionsText != null) nanodegreesTextView.setText(selectionsText);
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,9 +63,7 @@ public class AddArticleActivity extends AppCompatActivity {
 
   @Override public boolean onOptionsItemSelected(MenuItem item) {
     if (item.getItemId() == R.id.action_cancel) {
-      Intent intent = new Intent(this, FeedActivity.class);
-      startActivity(intent);
-      finish();
+      startFeedActivity();
       return true;
     }
     return super.onOptionsItemSelected(item);
@@ -70,27 +81,80 @@ public class AddArticleActivity extends AppCompatActivity {
     }
   }
 
+  private void startFeedActivity() {
+    Intent intent = new Intent(this, FeedActivity.class);
+    startActivity(intent);
+    finish();
+  }
+
   @OnClick(R.id.nanodegrees)
   public void showNanodegreesDialog(View view) {
     nanodegreesDialog = new MaterialDialog.Builder(this)
         .title(R.string.dialog_nanodegrees_title)
         .items(R.array.nanodegree_titles)
-        .itemsCallbackMultiChoice(selections, new MaterialDialog.ListCallbackMultiChoice() {
+        .itemsCallbackMultiChoice(selectionIndices, new MaterialDialog.ListCallbackMultiChoice() {
           @Override public boolean onSelection(MaterialDialog materialDialog, Integer[] integers,
               CharSequence[] values) {
             StringBuilder builder = new StringBuilder();
             for (int i = 0, n = values.length; i < n; i++) {
               builder.append(values[i]);
-              if (!(i == n - 1)) builder.append(", ");
+              if (!(i == n - 1)) builder.append(DELIMITER);
             }
             selectionsText = builder.toString();
-            nanodegrees.setText(selectionsText);
-            selections = integers;
+            nanodegreesTextView.setText(selectionsText);
+            selectionIndices = integers;
             return true;
           }
         })
         .positiveText(R.string.dialog_nanodegrees_positive_text)
         .negativeText(R.string.dialog_nanodegrees_negative_text)
         .show();
+  }
+
+  @OnClick(R.id.fab)
+  public void save(View view) {
+    final String title = titleEditText.getText().toString();
+    final String articleUrl = articleUrlEditText.getText().toString();
+    final String imageUrl = imageUrlEditText.getText().toString();
+
+    Pair<Boolean, String> validation = validateForm(title, articleUrl, imageUrl, selectionIndices);
+    boolean validated = validation.first;
+
+    if (validated) {
+      String[] nanodegreeIds = getResources().getStringArray(R.array.nanodegree_ids);
+      ArrayList<String> nanodegrees = new ArrayList<>();
+      for (int i = 0, n = selectionIndices.length; i < n; i++) {
+        nanodegrees.add(nanodegreeIds[selectionIndices[i]]);
+      }
+
+      Article article = new Article();
+      article.put(ParseConstants.ARTICLES_COL_TITLE, title);
+      article.put(ParseConstants.ARTICLES_COL_LINK, articleUrl);
+      article.put(ParseConstants.ARTICLES_COL_IMAGE_URL, imageUrl);
+      article.put(ParseConstants.ARTICLES_COL_NANODEGREES, nanodegrees);
+      article.saveEventually();
+
+      startFeedActivity();
+    } else {
+      Snackbar.make(view, validation.second, Snackbar.LENGTH_LONG).show();
+    }
+  }
+
+  private Pair<Boolean, String> validateForm(String title, String articleUrl, String imageUrl,
+      Integer[] indices) {
+    Resources res = getResources();
+    if (title == null || title.isEmpty()) {
+      return new Pair<>(false, res.getString(R.string.validation_enter_title));
+    } else if (articleUrl == null || articleUrl.isEmpty() ||
+        !Patterns.WEB_URL.matcher(articleUrl).matches()) {
+      return new Pair<>(false, res.getString(R.string.validation_invalid_article_url));
+    } else if (imageUrl != null && !imageUrl.isEmpty() &&
+        !Patterns.WEB_URL.matcher(imageUrl).matches()) {
+      return new Pair<>(false, res.getString(R.string.validation_invalid_image_url));
+    } else if (indices == null || indices.length == 0) {
+      return new Pair<>(false, res.getString(R.string.validation_select_nanodegree));
+    } else {
+      return new Pair<>(true, "");
+    }
   }
 }
